f7c5679020820415fb9de284ac7f2e08a80c2785
Parson, Robert <wrb176@mocs.utc.edu>
2020-01-13 14:22:30 -0500

    Added GetSelectedValue and refactored to use ternary conditions and lambdas

--- SeleniumMetabot/SeleniumGetMethods.cs
+++ SeleniumMetabot/SeleniumGetMethods.cs
@@ -41,6 +41,33 @@ namespace SeleniumMetabot
             return str;
         }
 
+        public static string GetSelectedValue(string elementType, string element)
+        {
+            string str = string.Empty;
+            elementType = Regex.Replace(elementType, @"s", "");
+            try
+            {
+                IWebElement webElement = ElementHelper.WebElement(elementType, element);
+                SelectElement selectedValue = new SelectElement(webElement);
+                str = selectedValue.SelectedOption.Text;
+                MethodSuccess = true;
+            }
+            catch (Exception e)
+            {
+                if (doTakeScreenshot)
+                {
+                    ScreenShot.TakeScreenShot();
+                }
+                MethodSuccess = false;
+                str = SeleniumUtilities.MethodName() + ":  " + "Message:  " + e.Message + Environment.NewLine +
+                    "Source:  " + e.Source + Environment.NewLine +
+                    "StackTrace:  " + e.StackTrace + Environment.NewLine +
+                    "Inner Exception:  " + e.InnerException + Environment.NewLine +
+                    "Parameters:  elementType = " + elementType + " | element = " + element + Environment.NewLine;
+            }
+            return str;
+        }
+
         public static string GetText(string elementType, string element)
         {
             string str = string.Empty;
@@ -64,6 +91,47 @@ namespace SeleniumMetabot
                     "StackTrace:  " + e.StackTrace + Environment.NewLine +
                     "Inner Exception:  " + e.InnerException + Environment.NewLine +
                     "Parameters:  elementType = " + elementType + " | element = " + element + Environment.NewLine;
+            }
+            return str;
+        }
+
+        public static string iGetSelectedValue(string elementType, string element)
+        {
+            string str = string.Empty;
+            elementType = Regex.Replace(elementType, @"s", "");
+            try
+            {
+                Navigation.SwitchToDefaultFrame();
+                IList<IWebElement> iframes = driver.FindElements(By.XPath("//iframe"));
+
+                GetSelectedValue(elementType, element);
+                if (MethodSuccess == false)
+                {
+                    foreach (IWebElement iframe in iframes)
+                    {
+                        driver.SwitchTo().Frame(iframe);
+                        str = GetSelectedValue(elementType, element);
+                    }
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                if (doTakeScreenshot)
+                {
+                    ScreenShot.TakeScreenShot();
+                }
+                str = SeleniumUtilities.MethodName() + ":  " + "Error:  " + Environment.NewLine +
+                    "Message:  " + e.Message + Environment.NewLine +
+                    "Source:  " + e.Source + Environment.NewLine +
+                    "StackTrace:  " + e.StackTrace + Environment.NewLine +
+                    "Inner Exception:  " + e.InnerException + Environment.NewLine +
+                    "Parameters:  elementType = " + elementType + " | element = " + element + Environment.NewLine;
+            }
+            finally
+            {
+                Navigation.SwitchToDefaultFrame();
             }
             return str;
         }
--- SeleniumMetabot/SeleniumSetMethods.cs
+++ SeleniumMetabot/SeleniumSetMethods.cs
@@ -14,7 +14,7 @@ namespace SeleniumMetabot
     public class SeleniumSetMethods : SeleniumProperties
     {
         
-        public static string MoveToAndEnterText(string elementType, string element, string value)
+        public static string MoveToAndEnterText(string elementType, string element, string value, bool doClear = true)
         {
             string str = string.Empty;
             elementType = Regex.Replace(elementType, @"s", "");
@@ -23,6 +23,12 @@ namespace SeleniumMetabot
             {
                 Actions actions = new Actions(driver);
                 IWebElement webElement = ElementHelper.WebElement(elementType, element);
+
+                if (doClear == true)
+                {
+                    webElement.Clear();
+                }
+                
                 actions.MoveToElement(webElement).Click(webElement).SendKeys(value);
 
                 string elementValue = SeleniumGetMethods.GetInputValue(elementType, element);
@@ -55,7 +61,7 @@ namespace SeleniumMetabot
         }
 
 
-        public static string EnterText(string elementType, string element, string value)
+        public static string EnterText(string elementType, string element, string value, bool doClear = true)
         {
             string str = string.Empty;
             elementType = Regex.Replace(elementType, @"s", "");
@@ -63,6 +69,12 @@ namespace SeleniumMetabot
             {
 
                 IWebElement webElement = ElementHelper.WebElement(elementType, element);
+
+                if (doClear == true)
+                {
+                    webElement.Clear();
+                }
+                
                 webElement.SendKeys(value);
 
                 
@@ -104,7 +116,7 @@ namespace SeleniumMetabot
         /// <param name="elementType"></param>
         /// <param name="element"></param>
         /// <param name="value"></param>
-        public static string iEnterText(string elementType, string element, string value)
+        public static string iEnterText(string elementType, string element, string value, bool doClear = true)
         {
             string str = string.Empty;
             elementType = Regex.Replace(elementType, @"s", "");
@@ -114,13 +126,13 @@ namespace SeleniumMetabot
                 IList<IWebElement> iframes = driver.FindElements(By.XPath("//iframe"));
                 
 
-                EnterText(elementType, element, value);
+                EnterText(elementType, element, value, doClear);
                 if (MethodSuccess == false)
                 {
                     foreach(IWebElement iframe in iframes)
                     {
                         driver.SwitchTo().Frame(iframe);
-                        EnterText(elementType, element, value);
+                        EnterText(elementType, element, value, doClear);
                     }
                     
                 }
